63a6f3b1d560586b65b48a9d04a0201597947f34
Oleg Chursin <olegchursin@gmail.com>
2017-11-03 18:55:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+
+
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO!"
+  var lowercase = "hello!"
+  var loveYouGrandma = "I love you, Grandma."
+  if (lowercase.toLowerCase() === lowercase) {
+    return "I can\'t hear you!"
+  }
+}
